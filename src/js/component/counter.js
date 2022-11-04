022ffd2951fe3ef337fec18d56059536eb435e23
KGTH <kgthcruz94@gmail.com>
2022-11-04 20:27:19 +0000

    se crea contador

---
+++ src/js/component/counter.js
@@ -0,0 +1,86 @@
+import React from "react";
+
+
+
+const Counter =()=>{
+
+return (
+    <div className="row row-cols-md-7 g-2">
+    <div className="col">
+      <div className="card border-dark">
+        
+        <div className="card-body tbtn btn-dark">
+          <h5 className="card-title"><i class="fas fa-clock "></i></h5>
+          
+        </div>
+      </div>
+    </div>
+    <div className="col">
+      <div className="card border-dark">
+       
+        <div className="card-body btn btn-dark">
+          <h5 className="card-title">0</h5>
+          
+        </div>
+      </div>
+    </div>
+    
+    <div className="col">
+      <div className="card border-dark">
+       
+        <div className="card-body btn btn-dark">
+          <h5 className="card-title">0</h5>
+          
+        </div>
+      </div>
+    </div>
+    <div className="col ">
+      <div className="card border-dark">
+        
+        <div className="card-body btn btn-dark">
+          <h5 className="card-title">0</h5>
+          
+        </div>
+      </div>
+    </div>
+    <div className="col">
+      <div className="card border-dark">
+        
+        <div className="card-body btn btn-dark">
+          <h5 className="card-title">0</h5>
+          
+        </div>
+      </div>
+    </div>
+    <div className="col">
+      <div className="card border-dark">
+        
+        <div className="card-body btn btn-dark">
+          <h5 className="card-title">0</h5>
+          
+        </div>
+      </div>
+    </div>
+    <div className="col">
+      <div className="card border-dark">
+        
+        <div className="card-body btn btn-dark">
+          <h5 className="card-title">0</h5>
+          
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+           
+       
+
+
+)
+}
+
+  
+
+
+export default Counter ;
